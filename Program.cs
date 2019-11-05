9e5a9bcc802605512e99e5e1f12dcfed474596e6
Aram Virabyan <googlaram200@gmail.com>
2019-11-05 07:48:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, this is stupid application xD) But, stop!");
+            Console.WriteLine("Can u please enter u name ? :)");
+            string name_of_user = Console.ReadLine();
+            Console.WriteLine($"Wow, {name_of_user} is very beautiful name!");
+            Console.WriteLine($"Bye, {name_of_user}!");
+        }
+    }
+}
