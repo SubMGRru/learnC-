1109d88162d6a6a1c75c465c4274b3acbe7cc579
Aram Virabyan <googlaram200@gmail.com>
2019-11-05 14:37:09 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Threading;
 
 namespace ConsoleApp1
 {
@@ -6,11 +7,128 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, this is stupid application xD) But, stop!");
+            Console.WriteLine("Hello, this is stupid application, that help u create own graphics statements! xD) But, stop, lets try it!");
+            Thread.Sleep(5000);
+            Console.WriteLine("I am sorry, but for security reasons u should enter u work email, please enter u Gmail address:");
+            string email_of_user = Console.ReadLine();
             Console.WriteLine("Can u please enter u name ? :)");
-            string name_of_user = Console.ReadLine();
-            Console.WriteLine($"Wow, {name_of_user} is very beautiful name!");
-            Console.WriteLine($"Bye, {name_of_user}!");
+            string firstname_of_user = Console.ReadLine();
+            Console.WriteLine($"Wow, {firstname_of_user} is very beautiful name!");
+            Thread.Sleep(5000);
+            Console.WriteLine($"{firstname_of_user}, enter please u lastname:");
+            string lastname_of_user = Console.ReadLine();
+            Console.WriteLine($"Ok, u are {firstname_of_user} {lastname_of_user}, its good start for our first dialog, at the result i am know who are u, or not ?");
+            Thread.Sleep(5000);
+            Console.WriteLine("Maybe, we can play now at my favourite funny game ?");
+            Thread.Sleep(5000);
+            Console.WriteLine("Its easy. Its u chance to know my age!");
+            Thread.Sleep(5000);
+            Console.WriteLine("Say how are u old and i am say how i am old. Just put number:");
+            string age_of_user = Console.ReadLine();
+            Console.WriteLine($"Siriusly ? U are {age_of_user} years old ? U are young!");
+            Thread.Sleep(5000);
+            Console.WriteLine("But, i am sorry, but.. my age is 100500, u likes this answer ?)");
+            Thread.Sleep(7000);
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Thread.Sleep(1000);
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine("What ??!?!?");
+            Console.WriteLine(" ");
+            Thread.Sleep(1000);
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine("Oh no! Seems... Someone hacked u computer! I can help u! Just say me u city and i am try to repair u desktop from viruses! Fast!");
+            string city_of_user = Console.ReadLine();
+            Console.WriteLine("Oh no! Seems.. I am need access to u Google account, becouse without it i cant help u! Just enter u password, without u login:");
+            string password_of_user = Console.ReadLine();
+            Console.WriteLine("Thanks! Wait one moment.. i am sending u password and email from Google account to my bussiness partner, he is paying me $0.90 for every getted Google Russian's account, and use it in darkside of Internet. ");
+            Thread.Sleep(13000);
+            Console.WriteLine("Connecting to.. 51.91.138.135:1500");
+            Thread.Sleep(3000);
+            Console.WriteLine("Sending login creditension to server...");
+            Thread.Sleep(5000);
+            Console.WriteLine("Wants look at datas that i am getted about u ? I am need it. Its help me restore access to u account, if u change password. With u datas i can stay owner of account without end! Now its my account :)");
+            Console.WriteLine("Look!");
+            Thread.Sleep(2000);
+            int a = Convert.ToInt32(age_of_user);
+            int year = 2019 - a;
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine($"|  Name   | ---> {firstname_of_user} <---");
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine($"|Lastname | ---> {lastname_of_user} <---");
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine($"|Birthday | ---> {year} ({age_of_user})<---");
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine($"|  City   | ---> {city_of_user} <---");
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine($"|  Email  | ---> {email_of_user} <---");
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine($"|Password | ---> {password_of_user} <---");
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("ENTER ANY WORD-SUMBOL FOR CONTINUE!!!!");
+            string stupid_word_of_user = Console.ReadLine();
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine($"{stupid_word_of_user}. Oh, bye! And i am have some idea for u.. register new account :0");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
